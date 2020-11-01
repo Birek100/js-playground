1da70e66520bef14174f28509b1f9bb65c144e85
Bartosz Szura <bartosz.szura100@gmail.com>
2020-11-01 15:56:17 +0100

    update

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-for (i=1; i<20; i++) 
-	if (i%2 == 0) {console.log(i + ' jest parzysta')} else {console.log(i + ' jest nieparzysta')}
+for (let i=1; i<20; i++) 
+	if (i%2 == 0) 
+		{console.log(i + ' jest parzysta')}
+	else 
+		{console.log(i + ' jest nieparzysta')};
