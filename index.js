70c3da8eb16e0becc7374e9ff51bd4d8e79232a8
Bartosz Szura <bartosz.szura100@gmail.com>
2020-11-06 10:14:26 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-for (let i=1; i<20; i++) 
+function oddeven () {for (let i=1; i<20; i++) 
 	if (i%2 == 0) 
 		{console.log(i + ' jest parzysta')}
 	else 
 		{console.log(i + ' jest nieparzysta')};
+}
+
+module.exports = oddeven
--- index2.js
+++ index2.js
@@ -1,3 +1,4 @@
+
 const array = []
 function randomNumber(min, max) {
 	min = Math.ceil(min);
@@ -5,6 +6,8 @@ function randomNumber(min, max) {
   	return  Math.floor(Math.random() * (max - min)) + min;
 }
 for (let i=1; i<=100; i++) {array.push(randomNumber(1,1001))}
-let result = Math.max(...array)
+let result = function(arg) {Math.max(arg)};
 console.log(array);
-console.log('The highest number is ' + result);
+console.log('The highest number is ' + result(5, 19));
+
+module.exports = result
