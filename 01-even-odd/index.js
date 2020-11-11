57435d801681e782b6b8eb984c02b8509c2afdc4
Bartosz Szura <bartosz.szura100@gmail.com>
2020-11-11 15:33:53 +0100

    update

--- 01-even-odd/index.js
+++ 01-even-odd/index.js
@@ -1,8 +1,10 @@
-function oddeven (x, y) {for (let x=1; x<y; x++) 
-	if (x%2 == 0) 
-		{console.log(x + ' is even')}
-	else 
-		{console.log(x + ' is odd')};
+function oddeven(x, y) {
+  for (let x = 1; x < y; x++)
+    if (x % 2 == 0) {
+      console.log(x + ' is even');
+    } else {
+      console.log(x + ' is odd');
+    }
 }
-oddeven(1, 20)
-module.exports = oddeven
+oddeven(1, 20);
+module.exports = oddeven;
