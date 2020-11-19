3bc3cf60dc087ef9b66520f489e8dcaf477c90de
Bartosz Szura <bartosz.szura100@gmail.com>
2020-11-19 10:55:04 +0100

    update

--- 03-bubble-sort/index.js
+++ 03-bubble-sort/index.js
@@ -1,18 +1,17 @@
 const array = [15, 8, 3, 1, 21, 17, 9]
-function bubbel(arr){
+function bubble(arr){
 	for (let i = 0; i < arr.length - 1; i++){
 		for (let j = 0 ; j < arr.length -1 - i; j++){
 			if (arr[j] > arr [j + 1]) {
-				let tmp = arr[j];
+				const tmp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = tmp;
 		    }
 		    
 		}
 	}
-	console.log(arr);
 	return arr;
 }
-bubbel(array);
+bubble(array);
 
-module.exports = bubbel;
+module.exports = bubble;
