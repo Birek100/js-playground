fa40a1ec66d841cefe651fc74bb7ca1bd2a042fb
Bartosz Szura <bartosz.szura100@gmail.com>
2020-12-18 10:52:43 +0100

    update

--- 04-data-tree/index.js
+++ 04-data-tree/index.js
@@ -7,9 +7,23 @@ class Node {
   addChild(child) {
     this.children.push(child);
   }
+  find(value) {
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].value == value) {
+        console.log(this.children[i]);
+        return this.children[i];
+      } else if (this.children[i] == this.children[this.children.length - 1]) {
+        for (let n = 0; n < this.children.length; n++) {
+          this.children[n].find(value);
+        }
+      } else {
+        console.log(this.children[i].value);
+      }
+    }
+  }
 }
 
-const root = new Node();
+const root = new Node('val');
 const node1 = new Node('value-1');
 const node2 = new Node('value-2');
 const node3 = new Node('value-3');
@@ -20,13 +34,11 @@ const node7 = new Node('value-7');
 
 root.addChild(node1);
 root.addChild(node2);
-node1.addChild(node3);
-node1.addChild(node4);
+node2.addChild(node3);
+node2.addChild(node4);
 node4.addChild(node5);
 node4.addChild(node6);
 node5.addChild(node7);
 
-console.log(root);
-console.log(node1);
-console.log(node4);
-console.log(node5);
+let result = root.find('value-7');
+console.log(result);
