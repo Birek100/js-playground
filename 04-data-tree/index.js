4feed08e62ba689fb96b4ae5821be06621962efa
Bartosz Szura <bartosz.szura100@gmail.com>
2020-12-24 12:49:59 +0100

    update

--- 04-data-tree/index.js
+++ 04-data-tree/index.js
@@ -8,7 +8,7 @@ class Node {
     this.children.push(child);
   }
 
-  findval(value) {
+ /* findval(value) {
 
   	for (const element of this.children) {
   		if (element.value == value) {
@@ -18,23 +18,26 @@ class Node {
   		console.log(element)
   		element.findval(value);
 	}
-  }
+  } */
 
- /* find(value) {
+  find(value) {
     for (let i = 0; i < this.children.length; i++) {
       if (this.children[i].value == value) {
-      	
-      	 //this.output = "test"
-      	 
-      
-      } else if (this.children[i] == this.children[this.children.length - 1]) {
+      	console.log('found')
+      }
+      else if (this.children[this.children.length - 1] == this.children[i] && this.children[this.children.length -1].value != value) {
+      	console.log(this.children[i])
         for (let n = 0; n < this.children.length; n++) {
-          this.children[n].find(value);
-        }
-      
+        	
+          this.children[n].find(value); 
+        } 
+    } 
+        else {
+      	console.log(this.children[i])
       }
     }
-  } */
+  } 
+
 }
 
 const root = new Node('val');
@@ -45,6 +48,7 @@ const node4 = new Node('value-4');
 const node5 = new Node('value-5');
 const node6 = new Node('value-6');
 const node7 = new Node('value-7');
+const node8 = new Node('value-8');
 
 root.addChild(node1);
 root.addChild(node2);
@@ -53,6 +57,8 @@ node2.addChild(node4);
 node4.addChild(node5);
 node4.addChild(node6);
 node5.addChild(node7);
+node1.addChild(node8);
 
-let result = root.findval('value-7');
-console.log(result);
+//let result = 
+root.find('value-8');
+//console.log(root);
