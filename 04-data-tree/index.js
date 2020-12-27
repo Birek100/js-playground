7dcd07e4bb5a654053a621a7e495c44a877b6c62
Bartosz Szura <bartosz.szura100@gmail.com>
2020-12-27 11:29:31 +0100

    update

--- 04-data-tree/index.js
+++ 04-data-tree/index.js
@@ -1,30 +1,23 @@
-let res = [];
-
 class Node {
   constructor(value) {
     this.value = value;
     this.children = [];
-    
   }
 
   addChild(child) {
     this.children.push(child);
   }
 
-findNode(node) {
-	console.log(this.value);
-	if (this.value == node.value) {
-		console.log('found')
-		return;
-	}
-	else {
-  		for (let i = 0; i < this.children.length; i++) {
-  			
-  			this.children[i].findNode(node)
-  		}
-
-  		}
-	}
+  findNode(node, callback) {
+    console.log(this.value);
+    if (this.value == node.value) {
+      callback(node);
+    } else {
+      for (let i = 0; i < this.children.length; i++) {
+        this.children[i].findNode(node);
+      }
+    }
+  }
 }
 
 const root = new Node('val');
@@ -46,45 +39,4 @@ node4.addChild(node6);
 node5.addChild(node7);
 node1.addChild(node8);
 
-
-root.findNode(node2);
-
-
-/* findval(value) {
-
-  	for (const element of this.children) {
-  		if (element.value == value) {
-  			console.log(true)
-  			//break;
-  		}
-  		console.log(element)
-  		element.findval(value);
-	}
-  } */
-
-
-  /* findVal(value) {
-    for (let i = 0; i < this.children.length; i++) {
-      if (this.children[i].value == value) {
-      	console.log(this.children[i].value)
-      	//return this.children[i].value;
-      	//res = this.children[i].value
-      	res.push(this.children[i]);
-      	  //this.children.find((c) => { return this.result = c == value; console.log(c)});
-      	
-      	
-      }
-      else if (this.children[this.children.length - 1] == this.children[i] && this.children[this.children.length -1].value != value) {
-      	console.log(this.children[i])
-        for (let n = 0; n < this.children.length; n++) {
-        	
-          this.children[n].findVal(value); 
-        } 
-    } 
-        else {
-      	console.log(this.children[i])
-      }
-
-    }
-
-  } */
+root.findNode(node7);
